aa43282717003f2a8488a0a2a14403b5b5dc6036
SaumyajitMishra <saumyajit97.mishra@gmail.com>
2023-05-04 17:30:04 +0530

    FirstCommit

---
+++ src/com/p1/A.java
@@ -0,0 +1,10 @@
+package com.p1;
+
+public class A {
+    public static void main(String[] args) {
+        String str = "SaumyajitMishra";
+          for(int i=str.length()-1;i>0;i--){
+    System.out.println(str.charAt(i));
+          }
+    }
+}
